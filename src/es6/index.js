f126636a3e4b5a4d339e2bf12f4e30c180a12495
Diego Camino <diego.camino.r@gmail.com>
2020-04-02 01:52:57 -0500

    Clases, Módulos y Generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -106,3 +106,35 @@ const helloPromise = () => {
 helloPromise()
   .then(response => console.log(response))
   .catch(error => console.log(error));
+
+class calculator {
+  constructor() {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+const helloFunc = require('./module');
+console.log(helloFunc());
+
+// generators
+function* helloWorld() {
+  if (true) {
+    yield 'Hello, ';
+  }
+  if (true) {
+    yield 'World';
+  }
+};
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value); // Hello,
+console.log(generatorHello.next().value); // World
+console.log(generatorHello.next().value); // undefined
