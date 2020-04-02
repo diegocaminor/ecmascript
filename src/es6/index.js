c0f9eb19b75262559fdb5af0fdc980cbaf723b85
Diego Camino <diego.camino.r@gmail.com>
2020-04-01 23:47:17 -0500

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,5 +1,4 @@
-////////////////
-////////////////
+// antes es6
 function newFunction(name, age, country) {
     var name = name || 'oscar';
     var age = age || 32;
@@ -15,12 +14,50 @@ function newFunction2(name='oscar', age=32, country='PE') {
 newFunction2();
 newFunction2('Diego', 26, 'AQP');
 
-////////////////
-////////////////
+// antes es6
 let hello = "Hello";
 let world = "World";
 let epicPhrase = hello + ' ' + world;
 console.log(epicPhrase);
-//es6
+// es6
 let epicPhrase2 = `${hello} ${world}` 
 console.log(epicPhrase2);
+
+// antes es6
+let lorem = "Frase épica de prueba \n"
+            + "otra frase épica de prueba";
+// es6
+let lorem2 = `Frase épica de prueba 
+otra frase épica de prueba`;
+console.log(lorem);
+console.log(lorem2);
+
+// antes es6
+let person = {
+    'name': 'oscar',
+    'age': 32,
+    'country': 'MX'
+}
+console.log(person.name, person.age, person.country);
+// es6
+let { name, age, country } = person;
+console.log(name, age, country);
+
+let team1 = ['Diego', 'Camila', 'Mack', 'Patrick']
+let team2 = ['Chacha', 'Jose', 'Tesla', 'Agni']
+let fullTeam = ['Veronica', 'Razcall', ...team1, ...team2]
+console.log(fullTeam);
+
+{
+    var globalVar = 'Gloabal var'; // var es una variable global
+}
+
+{
+    let globalLet = 'Gloabal let'; // let existe dentro de su scope
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = 'b'; // no puede cambiar el valor más adelante debido a que es una constante
+console.log(a);
