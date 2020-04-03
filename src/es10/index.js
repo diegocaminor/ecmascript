6b2bb20f10980fb6f553308dcf989c141c9beba9
Diego Camino <diego.camino.r@gmail.com>
2020-04-02 23:48:34 -0500

    Especificaciones con respecto a características nuevas en ES10

--- src/es10/index.js
+++ src/es10/index.js
@@ -1,5 +1,22 @@
 // ECMAScript 10(2019)
-// 
+// Array.flat()
+// Array.flat() devuelve una nueva matriz con cualquier sub-matriz (s) aplanada. Una llamada a Array.flat() sin ningún argumento solo aplanará un nivel de profundidad. Se puede proporcionar un argumento de profundidad opcional o simplemente se puede llamar de forma consecutiva.
+// Array.flatMap() El método flatMap() es idéntico al método de mapa ES6, pero también aplana el arreglo al mismo tiempo. El método flatMap() primero mapea cada elemento usando una función de mapeo, luego aplana el resultado en una nueva matriz. flatMap() es a menudo bastante útil, ya que fusionar ambos en un solo método es un poco más eficiente.
+// String.trimStart() & String.trimEnd() String.trimStart() se puede usar para recortar el espacio en blanco desde el inicio de una cadena.
+// Error opcional en catch permite a los desarrolladores usar try / catch sin el parámetro de error dentro del bloque catch.
+// Object.fromEntries() Crea un objeto o transforma pares clave-valor en un objeto. Solo acepta iterables, por ejemplo: Object.fromEntries(algunIterable).
+// Symbol.description La propiedad de descripción de solo lectura es una cadena que devuelve la descripción opcional de los objetos de Símbolo.
+
+let arr = [1, 2, 3, 4, 5];
+
+arr.map(x => [x, x * 2]);
+// [Array(2), Array(2), Array(2)]
+// 0: (2)[1, 2]
+// 1: (2)[2, 4]
+// 2: (2)[3, 6]
+
+arr.flatMap(v => [v, v * 2]);
+// [1, 2, 2, 4, 3, 6, 4, 8, 5, 10]
 
 let array = [1,2,3, [1,2,3, [1,2,3]]];
 
