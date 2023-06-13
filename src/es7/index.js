122269832eb913727c903f9ca386ce4a0c6cf99b
Diego Camino Reinoso <diego.camino.r@gmail.com>
2023-06-13 07:59:58 -0300

    Update ES7 file comments

--- src/es7/index.js
+++ src/es7/index.js
@@ -1,7 +1,9 @@
-// ecmascript7 nace en 2016
-// ECMAScript 2016(ES7) introduced only two new features:
-// Array.prototype.includes()
-// Exponentiation operator
+/* ecmascript7 nace en 2016
+ - ECMAScript 2016(ES7) introduced only two new features:
+ 1. Array.prototype.includes()
+ 2. Exponentiation operator
+ */
+
 let numbers = [1, 2, 3, 7, 8];
 
 if (numbers.includes(9)) {
