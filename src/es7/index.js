3dcaa344982f1852b5f3ca50c792fcd7d222d147
Diego Camino Reinoso <diego.camino.r@gmail.com>
2023-06-13 07:48:36 -0300

    Update es7 file

--- src/es7/index.js
+++ src/es7/index.js
@@ -5,9 +5,9 @@
 let numbers = [1, 2, 3, 7, 8];
 
 if (numbers.includes(9)) {
-  console.log('Si se encuentra el valor 7');
+  console.log("Si se encuentra el valor 7");
 } else {
-  console.log('No se encuentra.')
+  console.log("No se encuentra.");
 }
 
 let base = 4;
