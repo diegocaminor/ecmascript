025ade5dc697fc324bf9884dcf3b4fd3de154526
Diego Camino Reinoso <diego.camino.r@gmail.com>
2023-06-13 08:13:43 -0300

    Update ES9 file comments

--- src/es9/index.js
+++ src/es9/index.js
@@ -1,15 +1,16 @@
-// ECMAScript 9(2018)
-// Major new features:
-// Asynchronous Iteration (Domenic Denicola, Kevin Smith)
-// Rest/Spread Properties (Sebastian Markbåge)
-// New regular expression features:
-// RegExp named capture groups (Gorkem Yakin, Daniel Ehrenberg)
-// RegExp Unicode Property Escapes (Mathias Bynens)
-// RegExp Lookbehind Assertions (Gorkem Yakin, Nozomu Katō, Daniel Ehrenberg)
-// s (dotAll) flag for regular expressions (Mathias Bynens)
-// Other new features:
-// Promise.prototype.finally() (Jordan Harband)
-// Template Literal Revision (Tim Disney)
+/* ECMAScript 9(2018)
+ - Major new features:
+ 1. Asynchronous Iteration (Domenic Denicola, Kevin Smith)
+ 2. Rest/Spread Properties (Sebastian Markbåge)
+ 3. New regular expression features:
+ 4. RegExp named capture groups (Gorkem Yakin, Daniel Ehrenberg)
+ 5. RegExp Unicode Property Escapes (Mathias Bynens)
+ 6. RegExp Lookbehind Assertions (Gorkem Yakin, Nozomu Katō, Daniel Ehrenberg)
+ 7. (dotAll) flag for regular expressions (Mathias Bynens)
+ 8. Other new features:
+ 9. Promise.prototype.finally() (Jordan Harband)
+ 10. Template Literal Revision (Tim Disney)
+*/
 
 const obj = {
   name: "oscar",
